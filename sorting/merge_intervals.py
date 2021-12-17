a2ffd2648914a78aafdb8b793be6ca6e96c8b3df
Gadisa Amenu <amegadbay@gmail.com>
2021-12-17 17:26:46 +0300

    solved the problem of merging intervals on leetcode

---
+++ sorting/merge_intervals.py
@@ -0,0 +1,14 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        list_m = []
+        
+        intervals.sort(key=lambda x:x[0])
+        
+        for interval in intervals:
+            # if empty or no interval then simply append
+            if not list_m or list_m[-1][1] < interval[0]:
+                list_m.append(interval)
+            else: # it overlap exist, then update the latest end
+                list_m[-1][1] = max(list_m[-1][1], interval[1])
+        
+        return list_m
