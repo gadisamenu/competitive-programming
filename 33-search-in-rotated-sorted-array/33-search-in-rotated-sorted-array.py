fb34222e00baafa17c7a9fe4d83878b78d85cf20
Gadisa Amenu <86168126+gcodefree@users.noreply.github.com>
2022-03-02 00:30:36 +0300

    Time: 66 ms (39.23%), Space: 14.3 MB (79.31%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -0,0 +1,53 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        def k_finder():
+            frst_val = nums[0]
+            last_val = nums[-1]
+            left = 0
+            right = len(nums)-1
+            
+            while left < right-1:
+                mdl = left + (right-left)//2
+                val = nums[mdl]
+                
+                if nums[0]  < val and nums[-1] < val:
+                    left = mdl
+                elif nums[0] > val and nums[-1] > val:
+                    right = mdl
+                else:
+                    return right
+            return left
+        
+        
+        def  binarySearch(num,target):       
+            left = 0
+            right = len(num)-1
+
+            while left <= right:
+                mdl = left + (right-left)//2
+
+                peaked = num[mdl]
+
+                if peaked < target:
+                    left = mdl+1
+
+                elif peaked > target:
+                    right= mdl -1
+                else:
+                    return mdl
+            return None
+
+        k =k_finder()
+        
+        frst = binarySearch(nums[:k+1],target) 
+        scnd =binarySearch(nums[k+1:],target) 
+            
+        if frst != None:
+            return frst
+        if scnd != None:
+            return scnd +k + 1
+        return -1
+        
+        
+        
+                
