a022e313bcd180e2a9224a85e64b5d840255a784
Gadisa Amenu <86168126+gcodefree@users.noreply.github.com>
2022-03-03 23:55:42 +0300

    Time: 665 ms (66.00%), Space: 49.5 MB (62.09%) - LeetHub

---
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        que = deque([(root,1)])
+        
+        while que:
+            cur = que.popleft()
+            if not cur[0].left and not cur[0].right:
+                return cur[1]
+            if cur[0].left: que.append((cur[0].left,cur[1]+1))
+            if cur[0].right: que.append((cur[0].right,cur[1]+1))
+                
+        return cur[1]
