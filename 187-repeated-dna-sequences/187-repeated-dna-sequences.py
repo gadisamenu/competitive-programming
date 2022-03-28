0906e930ff1c928eaf70d7aa1d501da9f51f1273
Gadisa Amenu <86168126+gcodefree@users.noreply.github.com>
2022-03-28 21:38:00 +0300

    Time: 76 ms (78.78%), Space: 26.1 MB (60.95%) - LeetHub

--- 187-repeated-dna-sequences/187-repeated-dna-sequences.py
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.py
@@ -2,12 +2,12 @@ class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
         lgth = len(s)+1
         discovered = set()
-        ans = []
+        ans = set()
         i = 0 
         j = 10
         while j < lgth:
             if s[i:j] in discovered:
-                if s[i:j] not in ans: ans.append(s[i:j])
+                ans.add(s[i:j])
             else:
                 discovered.add(s[i:j])            
             i+=1
