10dc36f75c44ee51740f06b43d7666125bb1b2d0
Gadisa Amenu <86168126+gcodefree@users.noreply.github.com>
2022-07-31 21:56:21 +0300

    Time: 50 ms (58.37%), Space: 14 MB (27.37%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.py
+++ 29-divide-two-integers/29-divide-two-integers.py
@@ -1,22 +1,23 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-        quotient = 0 
+        if dividend == 0:
+            return 0
+        
+        if dividend == -2147483648 and divisor == -1:
+            return 2147483647
+        
+        
+        
         negative = True if (dividend > 0 and  divisor < 0) or (dividend < 0 and divisor > 0)  else False
         
         dividend = abs(dividend)
         divisor = abs(divisor)
         
+        quotient = 0 
         for i in range(31,-1,-1):
-            if divisor << i  <= dividend:
+            if divisor << i <= dividend:
                 dividend -= divisor << i
                 quotient += 1 << i
         
-        if negative: quotient *= -1
         
-            
-        if quotient > 2147483647:
-            return 2147483647
-        if quotient < -2147483648:
-            return -2147483648
-        
-        return quotient
+        return quotient * -1 if negative else quotient
