7050c78c953247ded7a01f715d6f6bfab4149545
Gadisa Amenu <86168126+gcodefree@users.noreply.github.com>
2022-08-02 20:21:21 +0300

    Time: 62 ms (78.07%), Space: 13.9 MB (36.15%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.py
+++ 12-integer-to-roman/12-integer-to-roman.py
@@ -1,35 +1,10 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
         ans = []
-        dividers  = {1000:"M",500:"D",100:"C",50:"L",10:"X",5:"V",1:"I"}
+        dividers  = {1000:"M",900:"CM",500:"D",400:"CD",100:"C",90:"XC",50:"L",40:"XL",10:"X",9:"IX",5:"V",4:"IV",1:"I"}
 
         for n in dividers:
-            
-            if 900 <= num < 1000:
-                ans.append("CM")
-                num -= 900
-                
-            elif 400 <= num < 500:
-                ans.append("CD")
-                num -= 400
-                
-            elif 90 <= num < 100:
-                ans.append("XC")
-                num -= 90
-                
-            elif 40 <= num < 50:
-                ans.append("XL")
-                num -= 40
-                
-            elif 9  == num:
-                ans.append("IX")
-                num -= 9
-                
-            elif num == 4:
-                ans.append("IV")
-                num -= 4
-           
-            elif num >=  n:
+            if num >=  n:
                 ans.append(dividers[n] * (num//n))
                 num  %= n
 
