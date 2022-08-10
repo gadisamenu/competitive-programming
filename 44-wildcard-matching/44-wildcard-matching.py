d00d2ea377fbec2ba886c5819d5786ebcd5a0105
Gadisa Amenu <86168126+gcodefree@users.noreply.github.com>
2022-08-11 01:44:36 +0300

    Time: 968 ms (55.44%), Space: 123 MB (18.08%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.py
+++ 44-wildcard-matching/44-wildcard-matching.py
@@ -1,21 +1,17 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:        
         @cache
-        def dp(s_ind:int,p_ind:int,star=False)-> bool:
+        def dp(s_ind:int,p_ind:int)-> bool:
             if  s_ind == len(s):
                 return True if p_ind == len(p) or "*"*(len(p) - p_ind) == p[p_ind:] else False
-             
             if p_ind == len(p): return False
             
+            star = True if p[p_ind] == "*" else False
+            
             if star:
-                return dp(s_ind+1,p_ind+1) or dp(s_ind +1,p_ind,True) or  dp(s_ind,p_ind+1)
+                return dp(s_ind+1,p_ind+1) or dp(s_ind +1,p_ind) or  dp(s_ind,p_ind+1)
 
             if s[s_ind] == p[p_ind] or p[p_ind] == "?":
                 return dp(s_ind+1,p_ind+1)
-            
-            if p[p_ind] == "*":
-                return dp(s_ind+1,p_ind,True) or  dp(s_ind,p_ind+1) or dp(s_ind+1,p_ind +1)
                 
-            
         return dp(0,0)
-    
