a37aa89f5ea22e987cafd8f2f39a4ba7d3681124
Gadisa Amenu <86168126+gcodefree@users.noreply.github.com>
2022-08-11 17:25:57 +0300

    Time: 2378 ms (62.26%), Space: 30.5 MB (43.40%) - LeetHub

---
+++ 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.py
@@ -0,0 +1,17 @@
+class Solution:
+    def replaceNonCoprimes(self, nums: List[int]) -> List[int]:
+        ans = [nums[0]]
+        ind = 1
+        while ind < len(nums):          
+            if ans:
+                _gcd =  gcd(ans[-1],nums[ind])
+                if _gcd > 1:
+                    nums[ind] = (nums[ind]*ans.pop())//_gcd
+                else:
+                    ans.append(nums[ind])
+                    ind += 1
+            else:
+                ans.append(nums[ind])
+                ind += 1
+        return ans
+
