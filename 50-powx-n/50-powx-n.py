4b0c0c2a7549f5cf50ecc9b4dc4136d0fd574c53
Gadisa Amenu <86168126+gcodefree@users.noreply.github.com>
2022-08-12 10:51:23 +0300

    Time: 48 ms (51.69%), Space: 13.9 MB (68.27%) - LeetHub

--- 50-powx-n/50-powx-n.py
+++ 50-powx-n/50-powx-n.py
@@ -7,4 +7,5 @@ class Solution:
         if n == -1:
             return 1/x
         val = self.myPow(x,n//2)
-        return val * val * self.myPow(x,(n%2))
+        odd =  x if n%2 else 1
+        return val * val * odd
