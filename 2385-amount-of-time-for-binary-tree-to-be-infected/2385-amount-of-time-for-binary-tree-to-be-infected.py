f81aefdc71a25b03275ec0f5f63e8ca569b359b0
Gadisa Amenu <86168126+gcodefree@users.noreply.github.com>
2022-08-22 21:22:39 +0300

    Time: 836 ms (75.00%), Space: 137.3 MB (75.00%) - LeetHub

--- 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.py
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.py
@@ -1,27 +1,28 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def init(self, val=0, left=None, right=None):
+#     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
 class Solution:
     def amountOfTime(self, root: Optional[TreeNode], start: int) -> int:
-        self.ans = 0
-        def recl(r):
-            if not r:
-                return [0,False]
+        self.time = 0
+        
+        def dfs(node):   
+            l_dis,l_flag = dfs(node.left) if  node.left else (0,False)
+            r_dis,r_flag = dfs(node.right) if node.right else (0,False)
+            if l_flag:
+                self.time = max(self.time,r_dis+l_dis+1)
+                return 1 +l_dis,l_flag
+            elif r_flag:
+                self.time = max(self.time,r_dis + l_dis + 1)
+                return 1 +r_dis,r_flag
             
-            right = recl(r.right)
-            left = recl(r.left)
-            if r.val == start:
-                self.ans = max(self.ans,max(right[0],left[0]))
-                return [1,True]
-            if right[1]:
-                self.ans = max(self.ans,right[0]+left[0])
-                return [right[0]+1,True]
-            if left[1]:
-                self.ans = max(self.ans,right[0]+left[0])
-                return [left[0]+1,True]
-            return [max(right[0],left[0])+1,False]
-        recl(root)
-        return self.ans
+            if node.val == start:
+                self.time = max(self.time,r_dis,l_dis)
+                return 0,True
+            
+            return max(l_dis,r_dis) +1 ,False
+        
+        dfs(root)
+        return self.time
