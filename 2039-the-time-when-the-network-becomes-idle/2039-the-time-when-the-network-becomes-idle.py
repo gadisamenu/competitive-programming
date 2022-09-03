86b3adcd360fc08a1d7bace0a29fe038ba38b32f
Gadisa Amenu <86168126+gcodefree@users.noreply.github.com>
2022-09-03 23:46:57 +0300

    Time: 4032 ms (50.85%), Space: 60.6 MB (99.44%) - LeetHub

--- 2039-the-time-when-the-network-becomes-idle/2039-the-time-when-the-network-becomes-idle.py
+++ 2039-the-time-when-the-network-becomes-idle/2039-the-time-when-the-network-becomes-idle.py
@@ -9,23 +9,20 @@ class Solution:
         min_cost = [-1 for _ in range(lgth)]
         visited = {0}
         queue = deque([(0,0)])
-        
+    
         while queue:
             cur,cst = queue.popleft()
-            min_cost[cur] = cst
+            min_cost[cur] = 2*cst
             
             for ngh in graph[cur]:
                 if ngh not in visited:
                     visited.add(ngh)
                     queue.append((ngh,cst+1))
-     
     
         ans = 0
-        for i in range(1,lgth):
-            t = 2*min_cost[i]
-            t = t //patience[i] if t %patience[i] else t //patience[i] -1
-            time = t* patience[i] + 2*min_cost[i]
-            ans = max(ans,time)
+        for i in range(1,lgth): 
+            time = min_cost[i] - (min_cost[i] % patience[i]) if min_cost[i] % patience[i] > 0 else min_cost[i] - patience[i] 
+            ans = max(ans,time + min_cost[i])
         return ans + 1
             
 
