42a929d8f286cf8d1a45bf91750fd689f6631013
Gadisa Amenu <86168126+gcodefree@users.noreply.github.com>
2022-09-15 14:41:54 +0300

    Time: 1789 ms (10.00%), Space: 16.8 MB (20.95%) - LeetHub

--- 954-array-of-doubled-pairs/954-array-of-doubled-pairs.py
+++ 954-array-of-doubled-pairs/954-array-of-doubled-pairs.py
@@ -1,16 +1,15 @@
 class Solution:
     def canReorderDoubled(self, arr: List[int]) -> bool:
         arr.sort()
-        doubles = defaultdict(list)
+        doubles = defaultdict(int)
         for n in arr:
             if doubles[n]:
-                doubles[n].pop()
+                doubles[n] -= 1
                 if not doubles[n]: doubles.pop(n)
             else:
                 doubles.pop(n)
                 if n < 0:
-                    doubles[n/2].append(n)
+                    doubles[n/2] += 1
                 else:
-                    doubles[2*n].append(n)
-                
+                    doubles[2*n] += 1
         return False if doubles else True
