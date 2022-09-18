ecc746b2e146401dbc083f747a62f22f8e1c0ea2
Gadisa Amenu <86168126+gcodefree@users.noreply.github.com>
2022-09-18 21:07:04 +0300

    Time: 327 ms (10.70%), Space: 16 MB (81.24%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -4,14 +4,16 @@ class Solution:
         _max = 0
         right_max =deque()
         for i in range(1,len(height)+1):
-            _max = max(height[-i],_max)
+            if height[-i] > _max: _max = height[-i]
             right_max.appendleft(_max)
             
         ans =0
         _max = 0
         for i in range(len(height)):
-            _max = max(_max,height[i])
-            ans += min(_max,right_max[i]) - height[i]
+            if height[i] > _max:_max = height[i]
+            ans -= height[i]
+            if _max < right_max[i]: ans += _max
+            else: ans += right_max[i] 
             
         return ans
             
