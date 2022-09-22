f7cf814a38f8ca6d4379d18b185062eb376596ef
Gadisa Amenu <86168126+gcodefree@users.noreply.github.com>
2022-09-22 20:38:23 +0300

    Time: 226 ms (5.05%), Space: 13.8 MB (69.34%) - LeetHub

--- 201-bitwise-and-of-numbers-range/201-bitwise-and-of-numbers-range.py
+++ 201-bitwise-and-of-numbers-range/201-bitwise-and-of-numbers-range.py
@@ -1,6 +1,6 @@
 class Solution:
     def rangeBitwiseAnd(self, left: int, right: int) -> int:
-        bits =[1 for i in range(32)]
+        ans = 0
         num = right
         j = 0
         for i in range(32):
@@ -8,13 +8,8 @@ class Solution:
             while num < left and  j < i:
                 num |= 1 << j
                 j+= 1
-            if num >= left:
-                bits[i] = 0
-            i += 1
-                
-        ans = 0
-        for i in range(32):
-            if bits[i]:
+            if num < left:
                 ans |= 1 << i
+            i += 1
     
         return ans
