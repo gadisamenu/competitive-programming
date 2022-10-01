500852fd11684373dafe71c55204a698b6250189
Gadisa Amenu <86168126+gcodefree@users.noreply.github.com>
2022-10-01 13:27:22 +0300

    Time: 31 ms (93.60%), Space: 13.9 MB (14.10%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.py
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.py
@@ -2,16 +2,16 @@ class Solution:
     def numTrees(self, n: int) -> int:
         
         @cache
-        def dfs(lv,hv):
+        def dp(lv,hv):
             
             if lv == hv: return 1
             if lv > hv:return 1
             chld = 0
             for i in range(lv,hv+1):
-                chld += dfs(lv,i-1) * dfs(i+1,hv)
+                chld += dp(lv,i-1) * dp(i+1,hv)
             return chld
         
-        return dfs(1,n)
+        return dp(1,n)
                 
     
             
