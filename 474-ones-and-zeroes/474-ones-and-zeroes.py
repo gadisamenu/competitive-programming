10484a8eaffc1ce73dd9fe00543698f77b02edec
Gadisa Amenu <86168126+gcodefree@users.noreply.github.com>
2022-10-06 12:02:41 +0300

    Time: 7366 ms (26.42%), Space: 14.2 MB (55.59%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.py
+++ 474-ones-and-zeroes/474-ones-and-zeroes.py
@@ -5,15 +5,24 @@ class Solution:
         for i in range(len(strs)):
             counts.append(Counter(strs[i]))
         
-        @cache
-        def dp(i, m, n):
-            if m < 0 or n < 0: return -float("inf")
-            if m == 0 and n == 0: return 0
-            if i >= len(strs):return 0
-            return max(1 + dp(i+1,m-counts[i]["0"], n-counts[i]["1"]),dp(i+1,m,n))
+#         @cache
+#         def dp(i, m, n): 
+#             if m < 0 or n < 0: return -float("inf")
+#             if m == 0 and n == 0: return 0
+#             if i >= len(strs):return 0
+#             return max(1 + dp(i+1,m-counts[i]["0"], n-counts[i]["1"]),dp(i+1,m,n))
         
-        return dp(0,m,n)
-            
+#         return dp(0,m,n)
+    
+        dp = [[0] * (n + 1) for i in range(m + 1)]
+        for i in range(len(strs)):
+            for j in range(m, -1, -1):
+                for k in range(n, -1, -1):
+                    if j - counts[i]["0"] >= 0 and k - counts[i]["1"] >= 0:
+                        dp[j][k] = max(dp[j][k], 1 + dp[j - counts[i]["0"]][k - counts[i]["1"]])
+        
+        # print(dp)
+        return dp[-1][-1]
             
             
             
