fe637b122d1c8a62977f9ff73f5610bd1bcfd080
Gadisa Amenu <86168126+gcodefree@users.noreply.github.com>
2022-10-07 14:39:43 +0300

    Time: 2725 ms (42.50%), Space: 13.9 MB (97.37%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -3,14 +3,27 @@ class Solution:
         _sum = sum(nums)
         if _sum % 2 == 1:return False
         _sum //= 2
-                
-        @cache
-        def dp(ind,sm):
-            if sm == _sum: return True
-            if sm > _sum: return False
-            if ind >= len(nums): return False
-            return dp(ind+1, sm + nums[ind]) or dp(ind+1,sm)
+        
+        dp = [ False for i in range(_sum+1)]
+        dp[0] = True
+        
+        for n in nums:
+            for i in range(len(dp)-1,-1,-1):
+                if dp[i]:
+                    if i + n <= _sum: dp[i+n] = True
+        return dp[-1]
+                    
             
-        return dp(0,0)
+                
+#         @cache
+#         def dp(ind,sm):
+#             if sm == _sum: return True
+#             if sm > _sum: return False
+#             if ind >= len(nums): return False
+#             return dp(ind+1, sm + nums[ind]) or dp(ind+1,sm)
+            
+#         return dp(0,0)
+
+
                 
             
