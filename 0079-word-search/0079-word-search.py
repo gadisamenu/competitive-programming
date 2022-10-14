3f8286100ca1f76aba942db70e34b949260c04d7
Gadisa Amenu <86168126+gcodefree@users.noreply.github.com>
2022-10-14 17:43:55 +0300

    Time: 1280 ms (95.95%), Space: 13.9 MB (50.91%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -14,8 +14,7 @@ class Solution:
         
         DIR = [0,1,0,-1,0]
         is_valid = lambda r,c,i : -1 < r < row and -1 < c < col and board[r][c] == word[i]
-    
-    
+        
     
         def dfs(r,c,i):
             if i+1 >= len(word): return True
