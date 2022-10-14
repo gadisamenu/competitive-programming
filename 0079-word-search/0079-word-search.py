454ed7dbb72cb880fd90680e7ba6b9db4350e415
Gadisa Amenu <86168126+gcodefree@users.noreply.github.com>
2022-10-14 15:21:10 +0300

    Time: 5494 ms (69.03%), Space: 13.9 MB (93.05%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,25 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        row = len(board)
+        
+        col = len(board[0])
+        DIR = [0,1,0,-1,0]
+        is_valid = lambda r,c,i : -1 < r < row and -1 < c < col and board[r][c] == word[i]
+    
+    
+        def dfs(r,c,i):
+            if i+1 >= len(word): return True
+            
+            board[r][c] = -1
+            for d in range(4):
+                n_r = r+DIR[d]
+                n_c = c + DIR[d+1]
+                if is_valid(n_r,n_c,i+1):
+                    if dfs(n_r,n_c,i+1):return True
+            board[r][c] = word[i]
+        
+        for r in range(row):
+            for c in range(col):
+                if board[r][c] == word[0]:
+                    if dfs(r,c,0): return True
+        return False
