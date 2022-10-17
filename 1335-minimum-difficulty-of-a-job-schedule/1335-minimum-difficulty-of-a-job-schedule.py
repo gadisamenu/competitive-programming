3c9da6e365bdb536bd28d8a7e4cc4c7cd5457d90
Gadisa Amenu <86168126+gcodefree@users.noreply.github.com>
2022-10-17 09:48:46 +0300

    Time: 153 ms (96.27%), Space: 20.9 MB (5.03%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
@@ -5,13 +5,14 @@ class Solution:
         
         @cache
         def dp(ind, dy,_max):
-            if ind == len(jobDifficulty): return _max if  not dy else float("inf")
+            if ind == len(jobDifficulty): 
+                return _max if  not dy else float("inf")
             
             if dy < 0: return float("inf")
             
             _max = max(_max,jobDifficulty[ind])
-            ind += 1
-            return min(_max+ dp(ind,dy-1,0), dp(ind,dy,_max))
+        
+            return min(_max+ dp(ind+1,dy-1,0), dp(ind+1,dy,_max))
         
         return dp(0,d,0)
         
