251f05d54b37383b3492a97f749e07acbe32b337
Gadisa Amenu <86168126+gcodefree@users.noreply.github.com>
2022-10-17 14:01:59 +0300

    Time: 2145 ms (23.59%), Space: 27.6 MB (48.08%) - LeetHub

--- 0848-shifting-letters/0848-shifting-letters.py
+++ 0848-shifting-letters/0848-shifting-letters.py
@@ -1,14 +1,12 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[int]) -> str:
         s = list(s)
+        
         for i in range(len(s)-1,0,-1):
             shifts[i-1]+=shifts[i]
-        letters = []
-        for i in range(97,123):
-            letters.append(chr(i))
-        
+            
         for i in range(len(s)):
-            s[i] = letters[((ord(s[i])-97)+shifts[i])%26]
+            s[i] = string.ascii_lowercase[((ord(s[i])-97)+shifts[i])%26]
             
         return "".join(s)
             
