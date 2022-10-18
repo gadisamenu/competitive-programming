3da5400dba3d91e6d56e10e0ed7b71d21c043a94
Gadisa Amenu <86168126+gcodefree@users.noreply.github.com>
2022-10-18 17:30:42 +0300

    Time: 66 ms (24.84%), Space: 14.4 MB (5.73%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
@@ -6,26 +6,25 @@ class Solution:
         
         next_direction = {"up":"right","down":"left","right":"down","left":"up"}
         next_cell = {"up":(-1,0),"down":(1,0),"right":(0,1),"left":(0,-1)}
-        
-        self.invalid = 0
+        self.break_point = n**2+1
         self.current_number = 1
         self.direction = "right"
+        
         def fill_cell(row,col):
+            if self.current_number == self.break_point:
+                return 
             matrix[row][col] = self.current_number
-            
+            self.current_number += 1
             next_row = row + next_cell[self.direction][0]
             next_col = col + next_cell[self.direction][1]
-
-            if isvalid_move(next_row,next_col):
-                self.invalid = 0
-                self.current_number += 1
-                fill_cell(next_row,next_col)
-            else:
-                self.invalid +=1
-                if self.invalid  > 1:return 
+            
+            if not isvalid_move(next_row,next_col):
                 self.direction=  next_direction[self.direction]
-                fill_cell(row,col)
+                next_row = row + next_cell[self.direction][0]
+                next_col = col + next_cell[self.direction][1]
                 
+            fill_cell(next_row,next_col)
+        
         fill_cell(0,0)
     
         return matrix
