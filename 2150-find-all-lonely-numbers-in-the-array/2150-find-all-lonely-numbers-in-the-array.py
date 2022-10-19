990fe9116f0020889ffd6177488c6d612fbacd83
Gadisa Amenu <86168126+gcodefree@users.noreply.github.com>
2022-10-19 17:58:16 +0300

    Time: 2702 ms (56.35%), Space: 29.7 MB (94.64%) - LeetHub

---
+++ 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findLonely(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        pre = 2
+        answer = []
+        for i in range(len(nums)-1):
+            difference = nums[i+1] - nums[i]
+            if difference > 1 and pre > 1:
+                answer.append(nums[i])
+            pre = difference
+        if pre > 1:answer.append(nums[-1])
+            
+        return answer
+            
+        
+                
+            
