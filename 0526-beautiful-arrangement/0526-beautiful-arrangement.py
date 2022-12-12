53046fb37f69f853afd51e7348e0db7f374355e7
Gadisa Amenu <86168126+gadisamenu@users.noreply.github.com>
2022-12-12 19:37:21 +0300

    Time: 181 ms (85.50%), Space: 18.1 MB (11.02%) - LeetHub

---
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countArrangement(self, n: int) -> int:
+        n+=1
+        @cache
+        def dp(index,visited):
+            if index == n:
+                return 1
+            answer = 0
+            
+            for  num  in range(1,n):
+                if (visited &1 <<  num ) == 0 and (index% num  == 0 or  num %index == 0):
+                    visited |= 1 << num 
+                    answer += dp(index + 1,visited)
+                    visited &= ~(1<<num)
+                    
+            return answer
+                            
+        return dp(1,0)
+            
+            
+            
