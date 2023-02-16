2d5b34ad6ed01c2412e693badda1d729a9b6e991
Gadisa Amenu <86168126+gadisamenu@users.noreply.github.com>
2023-02-16 23:50:43 +0300

    Time: 1702 ms (94.36%), Space: 15.3 MB (94.87%) - LeetHub

---
+++ 1531-string-compression-ii/1531-string-compression-ii.py
@@ -0,0 +1,34 @@
+class Solution:
+    def getLengthOfOptimalCompression(self, s: str, k: int) -> int:
+        def dp(start, k):
+            
+            if (start, k) in memo:
+                return memo[(start, k)]
+            
+            if start == n or n - start <= k:
+                return 0
+            
+            ans = float("inf")
+            count = defaultdict(int)
+            most_freq = 0
+            
+            for j in range(start, n):
+                c = s[j]
+                count[c] += 1
+                most_freq = max(most_freq, count[c])
+                
+                comp_len = 1 + (len(str(most_freq)) if most_freq > 1 else 0)
+                
+                prev = (j - start + 1 - most_freq)
+                if  k >= prev:
+                    ans = min(ans, comp_len + dp(j+1, k - prev))
+            
+            memo[(start, k)] = ans
+            
+            return ans
+        
+        
+        n = len(s)
+        memo = {}
+        
+        return dp(0, k)
