457db667a1b09e7ea2e96f4a79b9ea3524cad59c
Gadisa Amenu <86168126+gadisamenu@users.noreply.github.com>
2023-02-25 02:22:05 +0300

    Time: 45 ms (37.46%), Space: 14.1 MB (15.13%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
@@ -5,11 +5,13 @@ class Solution:
         def recursion(left, right):
             ans = []
             for i in range(left, right):
-                if expression[i] not in oper:
-                    continue
-                ans += [oper[expression[i]](lft, rgt) for lft in recursion(left, i)
-                                            for rgt in recursion(i + 1, right)]
-                
+                if expression[i] in oper:
+                    left_val = recursion(left,i)
+                    right_val = recursion(i+1,right)
+                    for lft in left_val:
+                        for rgt in right_val:
+                            ans.append(oper[expression[i]](lft,rgt))
+                            
             return ans if len(ans) != 0 else [int(expression[left:right])]
   
         return recursion(0, len(expression))
